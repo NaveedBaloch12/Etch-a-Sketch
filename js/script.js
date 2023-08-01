d319ad830c3a392f6f0cda17cc7604f7927218ce
Naveed <naveedhassankhan512@gamil.com>
2023-08-01 11:57:33 +0500

    Commit it

--- js/script.js
+++ js/script.js
@@ -26,6 +26,84 @@ function lightTheme() {
     document.body.style.color = "black"
 }
 
+
+
+// ======================================================================
+// ------------------------ artBoard ------------------------------------
+// ======================================================================
+
+// get no of div from range 
+document.getElementById("range").addEventListener('change', (e) => {
+    var noDiv = parseInt(e.target.value);
+    var numberOfDiv = 4 ** noDiv;
+    addDiv(numberOfDiv);
+});
+
+
+function addDiv(numberOfDiv) {
+    const artBoard = document.querySelector('#artBoard');
+    artBoard.innerHTML = '';
+
+    // Calculate the number of rows and columns needed for the grid
+    const cols = Math.sqrt(numberOfDiv);
+    const rows = cols;
+
+    // Calculate the width and height for each div
+    const containerWidth = artBoard.offsetWidth;
+    const divWidth = containerWidth / cols;
+    const divHeight = containerWidth / rows;
+
+    // Create the div and add them to the container
+    for (let i = 1; i <= numberOfDiv; i++) {
+        const div = document.createElement('div');
+
+        div.style.width = `${divWidth}px`;
+        div.style.height = `${divHeight}px`;
+
+        div.classList.add('content');
+
+        div.addEventListener('mousedown', () => {
+            isDragging = true;
+            div.style.backgroundColor = selectedColor;
+        });
+        div.addEventListener('mouseup', () => {
+            isDragging = false;
+        });
+        div.addEventListener('mousemove', () => {
+            if (isDragging) {
+                div.style.backgroundColor = selectedColor;
+            }
+        });
+
+        div.addEventListener('touchstart', () => {
+            isDragging = true;
+            div.style.backgroundColor = selectedColor;
+        });
+
+        div.addEventListener('touchend', () => {
+            isDragging = false;
+        });
+
+        div.addEventListener('touchmove', (event) => {
+            if (isDragging) {
+                event.preventDefault(); // Prevent page scrolling while dragging
+                const touch = event.touches[0];
+                const touchTarget = document.elementFromPoint(touch.clientX, touch.clientY);
+                if (touchTarget === div) {
+                    div.style.backgroundColor = selectedColor;
+                }
+            }
+        });
+
+        artBoard.appendChild(div);
+    }
+
+    // Set CSS Grid properties to create the grid layout
+    artBoard.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    artBoard.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+}
+
+
 // ======================================================================
 // ------------------- Active tool Class toggle -------------------------
 // ======================================================================
@@ -41,62 +119,19 @@ randomBtn.addEventListener('click', randomFunction);
 eraserBtn.addEventListener('click', eraserFunction);
 
 // get color code from color picker 
-document.getElementById("select-color").addEventListener('change', (e) => {
-    var selectedColor = e.target.value;
-})
-
+var selectedColor = "#000000"
 
 function penFunction() {
     console.log("Pen function");
-
+    document.getElementById("select-color").addEventListener('change', (e) => {
+        selectedColor = e.target.value;
+    })
 }
 
 function randomFunction() {
-    console.log("Random function");
+
 }
 
 function eraserFunction() {
-    console.log("eraser function");
 }
 
-
-// ======================================================================
-// ------------------------ artBoard ------------------------------------
-// ======================================================================
-// get no of div from range 
-document.getElementById("range").addEventListener('change', (e) => {
-    var noDiv = parseInt(e.target.value);
-    var numberOfDiv = 4 ** noDiv;
-    addDiv(numberOfDiv);
-  });
-
-  
-  function addDiv(numberOfDiv) {
-    const artBoard = document.querySelector('#artBoard');
-    artBoard.innerHTML = ''; // Clear existing divs
-  
-    // Calculate the number of rows and columns needed for the grid
-    const cols = Math.sqrt(numberOfDiv);
-    const rows = cols;
-  
-    // Calculate the width and height for each div
-    const containerWidth = artBoard.offsetWidth;
-    const divWidth = containerWidth / cols;
-    const divHeight = containerWidth / rows;
-  
-    // Create the div and add them to the container
-    for (let i = 1; i <= numberOfDiv; i++) {
-      const div = document.createElement('div');
-
-      div.style.width = `${divWidth}px`;
-      div.style.height = `${divHeight}px`;
-      
-      div.classList.add('content');
-      artBoard.appendChild(div);
-    }
-  
-    // Set CSS Grid properties to create the grid layout
-    artBoard.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-    artBoard.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-  }
-  
